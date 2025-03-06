e5680604467c4cebcd2891b5cc3a5658f079173b
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-03-06 10:33:32 +0900

    init: setup

---
+++ win/deploy.ps1
@@ -0,0 +1,80 @@
+# Script version
+$script_version = "1.0.0"
+
+function CreateBoxText() {
+  [CmdletBinding()]
+  Param (
+    [Parameter (ValueFromPipeline=$True)][string[]]$input_text,
+    [String[]]$Title,
+    [String[]]$Ver,
+    [String[]]$Url
+  )
+
+  Begin {
+    $CornerBoxChar = "+"
+    $HorizontalBoxChar = "-"
+    $VerticalBoxChar = "|"
+    $lines = @()
+
+
+    if ($input_text -eq $null) {
+      $input_text = $Title + $Ver + $Url
+    }
+  }
+
+  Process {
+    $maxLength = 0
+    $lineCount = 0
+
+    $input_text -split "`r`n" | ForEach-Object {
+      $lines += $_
+      If ($lines[$lineCount].Length -gt $maxLength) {
+        $maxLength = $lines[$lineCount].Length
+      }
+      $lineCount++
+    }
+  }
+
+  End {
+   $CornerBoxChar + ($HorizontalBoxChar * ($maxLength + 2)) + $CornerBoxChar
+   For ($i = 0; $i -lt $lineCount; $i++ ) {
+     if ($i -eq 1){
+       $VerticalBoxChar + (" " * ($maxLength - $lines[$i].Length - 1)) + $lines[$i] + "   " + $VerticalBoxChar
+     } else {
+       $VerticalBoxChar + " " + $lines[$i] + (" " * ($maxLength - $lines[$i].Length + 1))  + $VerticalBoxChar
+     }
+   }
+   $CornerBoxChar + ($HorizontalBoxChar * ($maxLength + 2)) + $CornerBoxChar
+  }
+}
+
+function greeting() {
+  "Atolycs Windows Setup Tools`r`nver: ${script_version}`r`nhttps://github.com/atolycs/setup-tools" | CreateBoxText
+  CreateBoxText "Atolycs Windows Setup Tools`r`nver: ${script_version}`r`nhttps://github.com/atolycs/setup-tools"
+
+  CreateBoxText -Title "Atolycs Windows Setup Tools" -Ver "ver: ${script_version}" -Url "https://github.com/atolycs/setup-tools"
+}
+
+info() {
+  Param(
+   [string[]]$message
+  )
+  "[ INFO ]" + $message
+}
+
+warn() {
+  Param(
+   [string[]]$message
+  )
+  "[ WARN ]" + $message
+}
+
+greeting
+
+# ReLaunch Administrator permission
+
+
+info "INFO TEST"
+warn "WARN TEST"
+
+
