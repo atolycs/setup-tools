ccc0a4d797188219764a21dda0b5476942a02f70
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-08-27 10:22:42 +0900

    feat: add machine setup script update

--- macos/machine-setup.sh
+++ macos/machine-setup.sh
@@ -1,17 +1,68 @@
 #!/usr/bin/env bash
 
+if [[ -t 1 ]]; then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+
+tty_mkbold() {
+  tty_escape "1;$1"
+}
+
+tty_underline="$(tty_escape "4;39")"
+tty_blue="$(tty_mkbold 34)"
+tty_red="$(tty_mkbold 31)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+
+say() {
+  printf "==> %s" $1
+}
+
+warn() {
+  printf "${tty_red}==>${tty_reset} %s\n" "$@"
+}
+
+ohai() {
+  printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
+}
+
+abort() {
+  printf "%s\n" "$@" >&2
+  exit 1
+}
+
+shell_join() {
+  local arg
+  printf "%s" "$1"
+
+  shift
+
+  for arg in "$@"; do
+    printf " "
+    printf "%s" "${arg// /\ }"
+  done
+}
+
 echo "Welcome macOS Setup tools"
 echo "   create by Atolycs 2025"
 
-echo "Setup Show extension"
+ohai "Checking os detection... => uname"
+OS="$(uname)"
+if [[ "${OS}" != "Darwin" ]]; then
+  abort "This script is only supported on macOS"
+fi
+
+say "Setup Show extension"
 
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-echo "Setup show path and status bar"
+say "Setup show path and status bar"
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
 
-echo "Setup Don't Create .DS_Store file on Network folder"
+say "Setup Don't Create .DS_Store file on Network folder"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-echo "Completed"
+say "Completed"
