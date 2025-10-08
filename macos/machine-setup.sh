abee69f403e9abecb308ef9a626584d7c073139c
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-10-08 09:55:26 +0900

    feat: update macos settings

--- macos/machine-setup.sh
+++ macos/machine-setup.sh
@@ -62,8 +62,23 @@ say "Setup show path and status bar"
 defaults write com.apple.finder ShowPathbar true
 defaults write com.apple.finder ShowStatusBar true
 
+say "Setup Finder configuration"
+defaults write com.apple.finder ShowHardDrivesOnDesktop 1
+defaults write com.apple.finder "_FXSortFoldersFirst" 1
+defaults write com.apple.finder ShowRecentTags 0
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
 say "Setup Don't Create .DS_Store file on Network folder"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+say "Setup Don't show recent app and file"
+defaults write com.apple.dock show-recents -bool false
+
+say "Setup Menu bar clock show Seconds"
+defaults write com.apple.menuextra.clock ShowSeconds 1
+
+say "Setup Click wallpaper to revelal desktop disable"
+defaults write "com.apple.WindowManager" EnableStandardClickToShowDesktop -bool false
 
 say "Completed"
 
