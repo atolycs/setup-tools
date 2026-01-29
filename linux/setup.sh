65ea68103f60511b7cf25ffe31293e5a8d4a879d
Atolycs <111569596+atolycs@users.noreply.github.com>
2026-01-29 15:59:04 +0900

    feat: add linux setup tool

---
+++ linux/setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "Welcome Atolycs Linux setup tool"
+
+echo "==> Detecting Operating System..."
+
+declare OS="unsupported os"
+
+if [ "$(uname)" == 'Darwin' ]; then
+  OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  RELEASE_FILE=/etc/os-release
+  if grep '^NAME="CentOS' "${RELEASE_FILE}" >/dev/null; then
+    OS=CentOS
+  elif grep '^NAME="Amazon' "${RELEASE_FILE}" >/dev/null; then
+    OS="Amazon Linux"
+  elif grep '^NAME="Ubuntu' "${RELEASE_FILE}" >/dev/null; then
+    OS=Ubuntu
+  else
+    echo "Your platform is not supported."
+    uname -a
+  fi
+elif [ "$(expr substr $(uname -s) 1 6)" == 'CYGWIN' ]; then
+  OS='Cygwin'
+else
+  echo "Your platform is not supported"
+  uname -a
+  exit 1
+fi
