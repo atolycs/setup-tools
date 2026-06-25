79cd3367280e1d9b92cc19ac7babf23b37c2626f
Atolycs <111569596+atolycs@users.noreply.github.com>
2026-06-25 11:22:22 +0900

    feat: add live setup

---
+++ linux/live.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source <(curl -fsSL https://raw.githubusercontent.com/atolycs/bash-util-closet/main/logs.func)
+
+msg_error "TEST"
+msg_info "ArchLinux Live Session SSH setup..."
+msg_ok "ArchLinux Live Session SSH setup..."
